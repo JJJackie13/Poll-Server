d1a5aa9a3e6d99c6e815b4cf5b7208b5ad22893b
JJJackie13 <jackyleung0103@gmail.com>
2022-01-30 21:48:38 +0800

    Update: socketIO

--- socketio.ts
+++ socketio.ts
@@ -2,11 +2,25 @@ import socketIO from "socket.io";
 
 export let io: socketIO.Server;
 
+export interface User {
+    userId?: number;
+    socketId?: string;
+}
+
 export function setSocketIO(value: socketIO.Server) {
     io = value;
     io.on("connection", async (socket) => {
         console.log("ALL SOCKET", await io.allSockets());
 
-        const {}
+        const { userId } = socket.handshake.auth;
+
+        let user: User = {
+            userId: userId,
+            socketId: socket.id,
+        }
+
+        if(userId){
+            
+        }
     })
 }
